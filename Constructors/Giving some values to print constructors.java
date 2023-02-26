bde53b44fe8ed7f5e5c57949fcef77e43e7a802c
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-02-26 18:15:53 +0530

    Add files via upload

---
+++ Constructors/Giving some values to print constructors.java
@@ -0,0 +1,13 @@
+package app_2; // giving some values to x y z and printing.
+
+public class B {
+	B(int x, char y, String z) {
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+	public static void main(String[] args) {
+		B a1 = new B(100, 'a',"mike");
+	}
+	}
+
