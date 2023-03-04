1a8b7726e6decbb8184bd8a4be9381f4d58b4fc7
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-04 23:51:11 +0530

    Update Constructor chaining-Ex-3.java

--- Constructor Chaining/Constructor chaining-Ex-3.java
+++ Constructor Chaining/Constructor chaining-Ex-3.java
@@ -1,16 +1,16 @@
 package app_2; 
 
 public class C {
-	int x;
+	int x; //non static variable
 	public static void main(String[] args) {
 		C a1 = new C();	
 		a1.test();
 		System.out.println(a1.x);
 		
 		}
-	public void test() {
-		int x=10;
-		this.x=x;
+	public void test() { //non static method
+		int x=10; //local variable
+		this.x=x; // "this.x" is calling non static variable (line #2), and "x" value copied from local variable.
 	}
 		}
 
