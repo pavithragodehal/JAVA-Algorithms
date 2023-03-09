b92ffeb6d033debdf6d4d8ac1d23272ecbad132d
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-09 23:21:11 +0530

    Add files via upload

---
+++ OOPs Concept in java/Inheritance/Ex-1/Class A.java
@@ -0,0 +1,5 @@
+package app_3;
+
+public class A {
+int x = 10;
+}
---
+++ OOPs Concept in java/Inheritance/Ex-1/Class B.java
@@ -0,0 +1,8 @@
+package app_3;
+
+public class B extends A {
+public static void main(String[] args) {
+	B b1 = new B();
+	System.out.println(b1.x);
+}
+}
