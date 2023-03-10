939b6d41fbe92f49fdd8201ce5b68498cd655d08
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-10 23:56:11 +0530

    Add files via upload

---
+++ Access specifiers/1.Default/EX-1 (same class)/same class.java
@@ -0,0 +1,14 @@
+package app_3;
+
+public class A {
+	int x =10;
+	void test() {
+	System.out.println(1000);
+	}
+	public static void main(String[] args) {
+		A a1 = new A();
+		System.out.println(a1.x);
+		a1.test();
+	}
+}
+
