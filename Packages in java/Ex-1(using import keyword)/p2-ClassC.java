f2a23c38f702c01f0ed1b5729af9e584aab3e7a9
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-11 23:31:55 +0530

    Add files via upload

---
+++ Packages in java/Ex-1(using import keyword)/p2-ClassC.java
@@ -0,0 +1,7 @@
+package p2;
+import p1.A; //we need to import if extending class from other package
+             // import packagename.classname
+             //maintain the same hierarchy of importing
+public class C extends A {
+
+}
