904fa1f92d043d9b945e440415a9c6563e44d6e2
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-11 22:58:52 +0530

    Update Class B.java

--- OOPs Concept in java/Inheritance/Ex-2/Class B.java
+++ OOPs Concept in java/Inheritance/Ex-2/Class B.java
@@ -1,9 +1,9 @@
-package app_3;
+package app_3;// child class
 
 public class B extends A {
 public static void main(String[] args) {
-	B b1 = new B();
+	B b1 = new B(); // In this object the value of "x" from class A is stored.
 	System.out.println(b1.x);
-	b1.test();
+	b1.test();// Method called from class A.
 }
 }
