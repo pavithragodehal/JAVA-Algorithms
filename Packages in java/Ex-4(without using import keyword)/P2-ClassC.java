9cddb132a2b677b13f6e8632493c80c0aeb00263
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-12 16:28:47 +0530

    Add files via upload

---
+++ Packages in java/Ex-4(without using import keyword)/P2-ClassC.java
@@ -0,0 +1,10 @@
+package p2;
+//did not import p1,but importing done with packageName.className
+public class C extends p1.A{
+ 
+	public static void main(String[] args) {
+		p1.A a1 = new p1.A();
+		System.out.println(a1);
+	}
+
+}
