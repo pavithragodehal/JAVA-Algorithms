64f7b0cbe0ffc251c2dc7f19362190fee0350bc4
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-12 16:42:03 +0530

    Add files via upload

---
+++ Packages in java/Ex-5(Package resolves naming convention problem)/p3-ClassB.java
@@ -0,0 +1,12 @@
+package p3;
+
+public class B {
+	public static void main(String[] args) {
+		//p1
+		p1.A a1 = new p1.A();
+		System.out.println(a1);
+		//p2
+		p2.A a2 = new p2.A();
+		System.out.println(a2);
+	}
+}
