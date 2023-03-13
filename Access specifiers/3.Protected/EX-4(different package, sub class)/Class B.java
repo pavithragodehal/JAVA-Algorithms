bfabe71e452304aed2a6acc5fa11e01d74970945
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:35:51 +0530

    Update Class B.java

--- Access specifiers/3.Protected/EX-4(different package, sub class)/Class B.java
+++ Access specifiers/3.Protected/EX-4(different package, sub class)/Class B.java
@@ -1,7 +1,7 @@
 package p2;
 //different package, sub class
 import p1.A;
-public class B extends A {
+public class B extends A{
 	public static void main(String[] args) {
 		B b1 = new B();
 		System.out.println(b1.x);
