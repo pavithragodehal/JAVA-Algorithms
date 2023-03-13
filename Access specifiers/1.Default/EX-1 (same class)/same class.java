12fa179325f8763dad1613c0df772017f89a6407
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:00:14 +0530

    Update same class.java

--- Access specifiers/1.Default/EX-1 (same class)/same class.java
+++ Access specifiers/1.Default/EX-1 (same class)/same class.java
@@ -1,14 +1,14 @@
 package app_3;
 
 public class A {
-	int x =10;
+	int x =10; //Access specifier = default (not writing anything before variable makes it default)
 	void test() {
 	System.out.println(1000);
 	}
 	public static void main(String[] args) {
 		A a1 = new A();
 		System.out.println(a1.x);
-		a1.test();
+		a1.test(); //No error,bcz if we make variable/method default,then it can be accessed only in same class, same package but not in different packages.
 	}
 }
 
