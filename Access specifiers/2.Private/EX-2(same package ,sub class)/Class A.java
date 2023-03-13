42c73594e58516d40516c41a992ee6e93c3656a8
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 01:33:11 +0530

    Add files via upload

---
+++ Access specifiers/2.Private/EX-2(same package ,sub class)/Class A.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class A {
+private int x = 10;
+private void test() {
+	System.out.println(1000);
+
+}
+}
---
+++ Access specifiers/2.Private/EX-2(same package ,sub class)/Class B.java
@@ -0,0 +1,10 @@
+package p1;
+//same package, sub class
+public class B extends A {
+	public static void main(String[] args) {
+		B a1 = new B();
+		System.out.println(a1.x);
+        a1.test();
+	}
+
+}
