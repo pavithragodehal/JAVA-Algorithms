fde6cdb1a06ffef58808785d3440da4b40490992
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:35:07 +0530

    Update Class B.java

--- Access specifiers/3.Protected/EX-5(different package, non-sub class)/Class B.java
+++ Access specifiers/3.Protected/EX-5(different package, non-sub class)/Class B.java
@@ -5,7 +5,7 @@ public class B {
 	public static void main(String[] args) {
 		A a1 = new A();
 		System.out.println(a1.x);
-                a1.test(); /*No error,bcz if we make variable/method default,then it can be accessed
+                a1.test(); /* Error,bcz if we make variable/method default,then it can be accessed
                              only in same class, same package but not in different packages.*/
 	}
 }
