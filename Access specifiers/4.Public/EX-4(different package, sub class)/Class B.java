716cdca6b695e0dc6bb8bb6d9b9cc073bc821049
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:50:40 +0530

    Update Class B.java

--- Access specifiers/4.Public/EX-4(different package, sub class)/Class B.java
+++ Access specifiers/4.Public/EX-4(different package, sub class)/Class B.java
@@ -1,10 +1,11 @@
-package p1;
-//same package, sub class
+package p2;
+//different package, sub class
+import p1.A;
 public class B extends A {
 	public static void main(String[] args) {
 		B b1 = new B();
 		System.out.println(b1.x);
-        b1.test(); /* if we make variable/method public, then it can be accessed everywhere.*/
+                b1.test(); /* if we make variable/method public, then it can be accessed everywhere.*/
 	}
 
 }
