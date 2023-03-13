5bac6d9ce2223858afaed252c22529c8ee3d2182
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 01:43:17 +0530

    Update Class B.java

--- Access specifiers/2.Private/EX-4(different package, sub class)/Class B.java
+++ Access specifiers/2.Private/EX-4(different package, sub class)/Class B.java
@@ -1,10 +1,11 @@
-package p1;
-//same package, sub class
+package p2;
+//different package, sub class
 public class B extends A {
 	public static void main(String[] args) {
-		B a1 = new B();
-		System.out.println(a1.x);
-        a1.test();
+		B b1 = new B();
+		System.out.println(b1.x);
+        b1.test(); //Error,bcz if we make variable/method private,then it can be accessed only in same class but not outside class.
+
 	}
 
 }
