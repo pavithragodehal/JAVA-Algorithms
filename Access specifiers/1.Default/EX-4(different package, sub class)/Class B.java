c1dbb8445914f9912f1b4e296423be91df306852
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:06:04 +0530

    Update Class B.java

--- Access specifiers/1.Default/EX-4(different package, sub class)/Class B.java
+++ Access specifiers/1.Default/EX-4(different package, sub class)/Class B.java
@@ -5,8 +5,8 @@ public class B extends A{
 		public static void main(String[] args) {
 			B a1 = new B();
 			System.out.println(a1.x);
-	        a1.test(); //error bcz it can be accessed in same class, same package but not in different packages.
-		
+	        a1.test(); //Error,bcz if we make variable/method default,then it can be accessed only in same class, same package but not in different packages.
+
 		}
 
 	}
