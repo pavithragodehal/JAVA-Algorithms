9cdae2e232af0775ad683d9fc4f5a3a0a010635d
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:06:58 +0530

    Update Class B.java

--- Access specifiers/1.Default/EX-5(different package, non-sub class)/Class B.java
+++ Access specifiers/1.Default/EX-5(different package, non-sub class)/Class B.java
@@ -1,11 +1,12 @@
 package p2;
 import p1.A;
 public class B {
-//different package, sub class
+//different package, non-sub class
 		public static void main(String[] args) {
 			A a1 = new A();
 			System.out.println(a1.x);
-	        a1.test(); //error bcz it can be accessed in same class, same package but not in different packages.
+	        a1.test(); //Error,bcz if we make variable/method default,then it can be accessed only in same class, same package but not in different packages.
+
 		}
 
 	}
