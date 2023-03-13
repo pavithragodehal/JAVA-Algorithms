67404e5e1580432ad85428feea25ca570256aefa
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:31:55 +0530

    Update Class B.java

--- Access specifiers/3.Protected/EX-4(different package, sub class)/Class B.java
+++ Access specifiers/3.Protected/EX-4(different package, sub class)/Class B.java
@@ -1,11 +1,12 @@
-package p1;
-//same package, sub class
+package p2;
+//different package, sub class
+import p1.A;
 public class B extends A {
 	public static void main(String[] args) {
-		B a1 = new B();
-		System.out.println(a1.x);
-        a1.test(); /*No error,bcz if we make variable/method default,then it can be accessed
-        only in same class, same package but not in different packages.*/
+		B b1 = new B();
+		System.out.println(b1.x);
+                b1.test(); /*No error,bcz if we make variable/method default,then it can be accessed
+                             only in same class, same package but not in different packages.*/
 	}
 
 }
