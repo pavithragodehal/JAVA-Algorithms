c3c64caeb06335938db7c6662678eabb14d73668
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:24:35 +0530

    Add files via upload

---
+++ Access specifiers/3.Protected/EX-2(same package, sub class)/Class A.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class A {
+protected int x = 10;
+protected void test() {
+	System.out.println(1000);
+
+}
+}
---
+++ Access specifiers/3.Protected/EX-2(same package, sub class)/Class B.java
@@ -0,0 +1,11 @@
+package p1;
+//same package, sub class
+public class B extends A {
+	public static void main(String[] args) {
+		B a1 = new B();
+		System.out.println(a1.x);
+        a1.test(); /*No error,bcz if we make variable/method default,then it can be accessed
+        only in same class, same package but not in different packages.*/
+	}
+
+}
