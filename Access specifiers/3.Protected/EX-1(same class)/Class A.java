6f6f856785406fb828500a11e6e0094c29fb25c0
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 02:11:00 +0530

    Update Class A.java

--- Access specifiers/3.Protected/EX-1(same class)/Class A.java
+++ Access specifiers/3.Protected/EX-1(same class)/Class A.java
@@ -9,6 +9,6 @@ protected void test() {
 public static void main(String[] args) {
 	A a1 = new A();
 	System.out.println(a1.x);
-    a1.test();
+    a1.test(); //No error,bcz if we make variable/method default,then it can be accessed only in same class, same package & in different packages only via inheritance
 }
 }
