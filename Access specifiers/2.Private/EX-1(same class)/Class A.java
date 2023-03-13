467b296c98e76f12afdb85b617f6080b4342d773
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-14 01:22:10 +0530

    Add files via upload

---
+++ Access specifiers/2.Private/EX-1(same class)/Class A.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class A {
+private int x = 10;
+private void test() {
+	System.out.println(1000);
+}
+public static void main(String[] args) {
+	A a1 = new A();
+	System.out.println(a1.x);
+    a1.test();
+}
+}
