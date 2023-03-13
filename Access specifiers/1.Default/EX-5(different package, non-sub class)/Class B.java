cd1e58d698498c343460783d7944e14977f24747
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-13 23:57:10 +0530

    Update Class B.java

--- Access specifiers/1.Default/EX-5(different package, non-sub class)/Class B.java
+++ Access specifiers/1.Default/EX-5(different package, non-sub class)/Class B.java
@@ -1,11 +1,11 @@
 package p2;
 import p1.A;
-public class B extends A{
+public class B {
 //different package, sub class
 		public static void main(String[] args) {
-			B a1 = new B();
+			A a1 = new A();
 			System.out.println(a1.x);
-	        a1.test();
+	        a1.test(); //error bcz it can be accessed in same class, same package but not in different packages.
 		}
 
 	}
