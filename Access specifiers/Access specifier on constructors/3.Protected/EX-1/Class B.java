a1ef3f57e51b51eb96d6b4f5ddd5cfb1cab6af2b
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-16 23:50:21 +0530

    Create Class B.java

---
+++ Access specifiers/Access specifier on constructors/3.Protected/EX-1/Class B.java
@@ -0,0 +1,7 @@
+package p1;//package #1
+public class B {
+public static void main(String[] args){
+   A a1 = new A(); /*Error,if we make a constructor protected, then it,s object can be created in 
+                       same package but not in different package,i.e same as default*/
+}
+}
