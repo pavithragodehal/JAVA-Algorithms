60b7337c98400698c3867e7386e0abf9105c4376
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-17 00:44:33 +0530

    Update Class B.java

--- Access specifiers/Access specifier on constructors/4.public/EX-1/Class B.java
+++ Access specifiers/Access specifier on constructors/4.public/EX-1/Class B.java
@@ -1,7 +1,6 @@
 package p1; //package #1
 public class B{
   public static void main (String[] args){
-    A a1 =new A();/* Error, if we make a constructor public, then it's object
-                     can be created in same package and in different package as well*/
+    A a1 =new A();
   }
 }
