357ce659ab72af15577a27dfc4c0cd8a1bcafc46
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-17 00:47:22 +0530

    Update Class B.java

--- Access specifiers/Access specifier on constructors/2.Default/EX-1/Class B.java
+++ Access specifiers/Access specifier on constructors/2.Default/EX-1/Class B.java
@@ -1,6 +1,6 @@
 package p1;//package #1
 public class B {
 public static void main(String[] args){
-   A a1 = new A(); //Error,if we make a constructor default, then it,s object can be created in same package but not outside package.
+   A a1 = new A(); 
 }
 }
