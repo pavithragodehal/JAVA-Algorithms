9484cf75deced837a15608a625ab77d8e149901b
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-16 23:57:56 +0530

    Create Class A.java

---
+++ Access specifiers/Access specifier on constructors/4.public/EX-1/Class A.java
@@ -0,0 +1,10 @@
+package p1; //package #1
+public class A{
+  public A(){
+    
+  }
+  public static void main (String[] args){
+    A a1 = new A();/* Error, if we make a constructor public, then it's object
+                     can be created in same package and in different package as well*/
+  }
+}
