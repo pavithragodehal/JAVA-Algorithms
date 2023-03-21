4e40c65b63700a4959702a43f3851791ff1ad887
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-21 13:33:05 +0530

    Update Class B.java

--- OOPs Concept in java/Polymorphism/Overriding/EX-4/Class B.java
+++ OOPs Concept in java/Polymorphism/Overriding/EX-4/Class B.java
@@ -1,14 +1,14 @@
 package p1;
 //polymorphism
 public class B extends A{
-	public void test() { //here test method is taking more than one form
-			System.out.println(500); //overriding 500 with 100 in class A
+	@override // To check whether override is happening or not.
+	public void test() { //Error
+			System.out.println(500); 
 	}
 		public static void main(String[] args) {
 			B b1 = new B();
-			b1.test();
+			b1.test1();
 		}
-
 	}
 	
 
