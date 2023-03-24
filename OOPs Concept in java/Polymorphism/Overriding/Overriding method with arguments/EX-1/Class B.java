43d1d01474536f7c0b5fba79c5c2f1058d3cae3f
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-24 14:54:37 +0530

    Update Class B.java

--- OOPs Concept in java/Polymorphism/Overriding/Overriding method with arguments/EX-1/Class B.java
+++ OOPs Concept in java/Polymorphism/Overriding/Overriding method with arguments/EX-1/Class B.java
@@ -1,12 +1,13 @@
 package p1;
 //polymorphism
 public class B extends A{
-	public void test() { //here test method is taking more than one form
-			System.out.println(500); //overriding 500 with 100 in class A
+	@override
+	public void test1(int y) { //argument:data type should match, variable name can be anything.
+			System.out.println(y); 
 	}
 		public static void main(String[] args) {
 			B b1 = new B();
-			b1.test();
+			b1.test1(800);
 		}
 
 	}
