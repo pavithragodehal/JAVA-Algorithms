1868bbfa9ecd7d6990e6da3f87ae94b1347185ad
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-03-24 15:09:08 +0530

    Update Class A.java

--- OOPs Concept in java/Polymorphism/Overriding/Overriding method with arguments/EX-3/Class A.java
+++ OOPs Concept in java/Polymorphism/Overriding/Overriding method with arguments/EX-3/Class A.java
@@ -1,7 +1,12 @@
 package p1; 
-public class A {
-	public void test() {
-		System.out.println(100);
+public class GoldAccount {
+	public void onlineBanking() {
+		System.out.println("yes");
 	}
-
+	public void chqBooks() {
+		System.out.println("2/year");
+	}
+        public void rateOfInterest() {
+		System.out.println("Nill");
+	}
 }
