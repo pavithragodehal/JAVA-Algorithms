865971156d464b9a19aa8ac68cdeadaab1756a9b
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-04-07 23:57:33 +0530

    Add files via upload

---
+++ OOPs Concept in java/Encapsulation/EX-1/Class A.java
@@ -0,0 +1,19 @@
+package p1; 
+public class A {
+	public void test() { //Method #1
+		System.out.println(100);
+	}
+	public void test(int x) { //Method #2
+		System.out.println(x);
+	}
+	public void test(String x) { //Method #3
+		System.out.println(x);
+	}
+public static void main(String[] args) { //Method #4
+	A a1 = new A();
+	a1.test();
+	a1.test(200);
+	a1.test("pankaj sir Academy");
+	
+}
+}
