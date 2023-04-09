8c8002c1bfdf7cae8b9776c9d4cc79116befb1f7
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-04-09 17:58:26 +0530

    Update Class E.java

--- OOPs Concept in java/Encapsulation/EX-3(multiple variables)/Class E.java
+++ OOPs Concept in java/Encapsulation/EX-3(multiple variables)/Class E.java
@@ -1,17 +1,32 @@
 package p1;
-//static initialization block (SIB)
 public class E {
-	static
-	{ 
-		System.out.println("From SIB-1");
-	}
-	static
-	{ 
-		System.out.println("From SIB-2"); //Multiple SIBs
-	}
-	public static void main(String[] args) {
-		System.out.println("From main");
-		//creating one main & SIB twice & see which is called first.
-	}
-
+	private int id;
+	private String name;
+	private String city:
+	private int firstInstallment;
+	//Right click >Source  >Generate Getters & setters...
+        public int getId() {
+	return id;
+        }  
+        public void setId(int id) {
+	this.id = id;
+        } 
+	public String  getName() {
+	return name;
+        }  
+        public void setName(String name) {
+	this.name = name;
+        } 
+	public String  getCity() {
+	return city;
+        }  
+        public void setCity(String city) {
+	this.city = city;
+        } 
+	 public int getFirstInstallment() {
+	return firstInstallment;
+        }  
+        public void setFirstInstallment(int firstInstallment) {
+	this.firstInstallment = firstInstallment;
+        } 
 }
