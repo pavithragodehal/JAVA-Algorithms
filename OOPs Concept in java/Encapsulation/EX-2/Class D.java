62167cf85c91f75505bd2beb5fa44451f5f1e56a
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-04-09 17:42:54 +0530

    Update Class D.java

--- OOPs Concept in java/Encapsulation/EX-2/Class D.java
+++ OOPs Concept in java/Encapsulation/EX-2/Class D.java
@@ -1,18 +1,12 @@
 package p1;
-//static initialization block (SIB)
 
 public class D {
-	static
-	 { //SIB
-		System.out.println("From SIB");
-	}
-	
-	{ //IIB
-		System.out.println("From IIB");
-	}
 	public static void main(String[] args) {
-		D a1 = new D();
-		//creating one SIB with IIB & see which is called first.
+		C c1 = new C();
+		c1.setName("Pavitra");
+		String name = c1.getName();
+		System.out.println(name);
+		
 	}
 
 }
