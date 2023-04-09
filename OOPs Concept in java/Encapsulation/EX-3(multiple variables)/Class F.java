715f325ab234d77590d404a94a5a439e9b339b38
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-04-09 18:08:09 +0530

    Update Class F.java

--- OOPs Concept in java/Encapsulation/EX-3(multiple variables)/Class F.java
+++ OOPs Concept in java/Encapsulation/EX-3(multiple variables)/Class F.java
@@ -1,28 +1,20 @@
 package p1;
-//static initialization block (SIB)
-
-
 public class F {
-	{
-		System.out.println("From IIB-1");
-	}
-	{
-		System.out.println("From IIB-2");
-	}
-	F(){
-		System.out.println("From constructor");
-	}
-	static
-	{
-		System.out.println("From SIB");
-	}
-	
-	public static void main(String[] args) {
-		F a1 = new F();
-        System.out.println("From main");
-		// To understand the flow b/w SIB,IIB-1,IIB-2,Constructor and Main
-	}
+		public static void main(String[] args) {
+			E e1 = newE();
+			e1,setId(672);
+			e1,setName("pavithra");
+			e1,setCity("Bengalore");
+			e1,setFirstInstallment(25000);
+                //direct printing value
+		System.out.println(e1.getId());
 
+		System.out.println(e1.getName());
+		
+		System.out.println(e1.getCity());
+			
+		System.out.println(e1.getFirstInstallment());
+	}
 }
 
 
