f3b242f6d752d87b8e4ff8655ab2f4a42e28b14a
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-04-09 16:59:55 +0530

    Update Class A.java

--- OOPs Concept in java/Encapsulation/EX-1/Class A.java
+++ OOPs Concept in java/Encapsulation/EX-1/Class A.java
@@ -1,19 +1,17 @@
 package p1; 
-public class A {
-	public void test() { //Method #1
-		System.out.println(100);
+//Encapsulation
+public class A { 
+	private int id; //non static
+	//Below method is "setter"
+	public void setId(int id) { // here "id" is local variable 
+		this.id = id; //with "this" we accessed non static member.
+		// so nonstaticId = localId although variables are same
 	}
-	public void test(int x) { //Method #2
-		System.out.println(x);
+		//Below method is "Getter"
+	private int getId() { //because void methods can't return value
+		return id;
 	}
-	public void test(String x) { //Method #3
-		System.out.println(x);
-	}
-public static void main(String[] args) { //Method #4
-	A a1 = new A();
-	a1.test();
-	a1.test(200);
-	a1.test("pankaj sir Academy");
+}
+//now we'll set & get id value in different class
+
 	
-}
-}
