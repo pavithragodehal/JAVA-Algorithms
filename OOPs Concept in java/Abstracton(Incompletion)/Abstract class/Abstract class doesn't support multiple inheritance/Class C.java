8d5488090759ae30bb4e13c8cabfdd9b49b0bcd5
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-05-16 00:32:28 +0530

    Update Class C.java

--- OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Abstract class doesn't support multiple inheritance/Class C.java
+++ OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Abstract class doesn't support multiple inheritance/Class C.java
@@ -1,23 +1,7 @@
 package p1;
-//static initialization block (SIB)
 
-public class C {
-	//IIB,SIB, and constructor Flow 
-	static { //SIB-1
-		System.out.println("From SIB-1");
-	}
-	static { //SIB-2
-		System.out.println("From SIB-2");
-		new C();
-	}
-		{ // IIB
-			System.out.println("From IIB");
-		}
-			
-			public static void main(String[] args) {
-				new C();
-				// Multiple object created, in SIB and Main.
-			}
+abstract public class C extends A,B{ //error
+	
 		}
 
 
