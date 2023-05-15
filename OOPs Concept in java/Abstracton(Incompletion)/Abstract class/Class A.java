9c053944bfaa8a1038323c55df2b4219efe7a829
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-05-15 23:58:20 +0530

    Update Class A.java

--- OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Class A.java
+++ OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Class A.java
@@ -1,19 +1,17 @@
 package p1; 
-public class A {
-	public void test() { //Method #1
+//Incomplete and complete methods
+abstract public class A {
+	int x =10;
+	static int y = 100;
+	A(){ //constructor
+	}
+	abstract public void test1(); //Incomplete Metod, using abstract keyword is mandatory in class.
+	public void test2 () { //complete method 
 		System.out.println(100);
 	}
-	public void test(int x) { //Method #2
-		System.out.println(x);
-	}
-	public void test(String x) { //Method #3
-		System.out.println(x);
-	}
-public static void main(String[] args) { //Method #4
-	A a1 = new A();
-	a1.test();
-	a1.test(200);
-	a1.test("pankaj sir Academy");
+	
+public static void main(String[] args) { //main method
+	A a1 = new A(); //error
 	
 }
 }
