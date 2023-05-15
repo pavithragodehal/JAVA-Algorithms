614cd78b6b13860d38604406f4f113be617400b8
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-05-16 00:38:24 +0530

    Update Class A.java

--- OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Inheritance from Abstract class to java class/Class A.java
+++ OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Inheritance from Abstract class to java class/Class A.java
@@ -1,19 +1,11 @@
 package p1; 
-public class A {
-	public void test() { //Method #1
-		System.out.println(100);
-	}
-	public void test(int x) { //Method #2
-		System.out.println(x);
-	}
-	public void test(String x) { //Method #3
-		System.out.println(x);
-	}
-public static void main(String[] args) { //Method #4
-	A a1 = new A();
-	a1.test();
-	a1.test(200);
-	a1.test("pankaj sir Academy");
+abstract public class A {
+	int X = 100;//non static member
+	static int y = 400;//static member
 	
-}
+	public void test() { // non static method
+		System.out.println(200);
+	}
+	abstract public void example();//abstract method
+	
 }
