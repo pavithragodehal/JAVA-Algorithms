1c0e9d58446f7629cf83b1338829574372e448f3
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-05-16 02:13:21 +0530

    Update Class B.java

--- OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Inheritance from Abstract class to java class/Class B.java
+++ OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Inheritance from Abstract class to java class/Class B.java
@@ -1,14 +1,18 @@
 package p1;
-//polymorphism
 public class B extends A{
-	public void test() { //here test method is taking more than one form
-			System.out.println(500); //overriding 500 with 100 in class A
-	}
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 			B b1 = new B();
-			b1.test();
-		}
+			b1.test(); //calling non static member
+			System.out.println(b1.x);//printing non static member
+		        b1.example();//calling overriden abstract method
+			System.out.println(A.y);//calling static member with ClassName
 
+		
+		}
+	@override
+	public  void example() { 
+	System.out.println(300);
 	}
+}
 	
 
