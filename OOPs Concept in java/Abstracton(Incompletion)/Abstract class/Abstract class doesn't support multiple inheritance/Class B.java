00a84f5b91ba1d372426997d9d4c97b8b1b80397
pavithragodehal <82913091+pavithragodehal@users.noreply.github.com>
2023-05-16 00:31:17 +0530

    Update Class B.java

--- OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Abstract class doesn't support multiple inheritance/Class B.java
+++ OOPs Concept in java/Abstracton(Incompletion)/Abstract class/Abstract class doesn't support multiple inheritance/Class B.java
@@ -1,13 +1,6 @@
 package p1;
-//polymorphism
-public class B extends A{
-	public void test() { //here test method is taking more than one form
-			System.out.println(500); //overriding 500 with 100 in class A
-	}
-		public static void main(String[] args) {
-			B b1 = new B();
-			b1.test();
-		}
+
+abstract public class B {
 
 	}
 	
